13b4fbf7ead5160356a5aafc6ce7d21deaf60787
sum0999 <sum@microsoft.com>
2019-07-25 13:44:42 +0530

    Update App.xaml.cs

--- PushReproXamarin17_5_2019-master/PushReproXamarin/PushReproXamarin/App.xaml.cs
+++ PushReproXamarin17_5_2019-master/PushReproXamarin/PushReproXamarin/App.xaml.cs
@@ -17,6 +17,33 @@ namespace PushReproXamarin
             MainPage = new MainPage();
 
             AppCenter.LogLevel = LogLevel.Verbose;
+            // This should come before AppCenter.Start() is called
+            // Avoid duplicate event registration:
+            if (!AppCenter.Configured)
+            {
+                Push.PushNotificationReceived += (sender, e) =>
+                {
+                    // Add the notification message and title to the message
+                    var summary = $"Push notification received:" +
+                                        $"\n\tNotification title: {e.Title}" +
+                                        $"\n\tMessage: {e.Message}";
+
+                    // If there is custom data associated with the notification,
+                    // print the entries
+                    if (e.CustomData != null)
+                    {
+                        summary += "\n\tCustom data:\n";
+                        foreach (var key in e.CustomData.Keys)
+                        {
+                            summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                        }
+                    }
+
+                    // Send the notification summary to debug output
+                    System.Diagnostics.Debug.WriteLine(summary);
+                    MainPage.DisplayAlert("Custom Data",summary,"OK");
+                };
+            }
             AppCenter.Start("<Your-App-Secret>", typeof(Push));            
         }
 
